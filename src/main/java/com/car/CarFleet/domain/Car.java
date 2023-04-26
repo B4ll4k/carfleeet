09d8c33be7139fc9aefd90c291402b5eb7d8d123
Beamlak <beamlakalemseged@gmail.com>
2023-04-26 07:12:27 -0500

    domain class done

--- src/main/java/com/car/CarFleet/domain/Car.java
+++ src/main/java/com/car/CarFleet/domain/Car.java
@@ -5,5 +5,52 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 public class Car {
+    @Id
+    private  String licensePlate;
+    private String type;
+    private String brand;
+    private double priceperDay;
+    private boolean isAvailable;
+
+    
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+
+
+    public String getType() {
+        return type;
+    }
+
+
+
+    public String getBrand() {
+        return brand;
+    }
+
+
+
+    public double getPriceperDay() {
+        return priceperDay;
+    }
+
+
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+
+
+    public Car(String licensePlate, String type, String brand, double priceperDay, boolean isAvailable) {
+        this.licensePlate = licensePlate;
+        this.type = type;
+        this.brand = brand;
+        this.priceperDay = priceperDay;
+        this.isAvailable = isAvailable;
+    }
+    
 
 }
