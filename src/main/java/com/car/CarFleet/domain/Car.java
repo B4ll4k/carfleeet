bbf3145a882eee04a4dc065986d9ab549b2747cb
Beamlak <beamlakalemseged@gmail.com>
2023-04-26 07:01:03 -0500

    first commit

---
+++ src/main/java/com/car/CarFleet/domain/Car.java
@@ -0,0 +1,9 @@
+package com.car.CarFleet.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Car {
+
+}
